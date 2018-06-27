393c1cfd76241939e40eb34380a260ae60c77e9f
ToshikatsuTanahashi <allneko.club.llc@gmail.com>
2018-06-27 20:50:09 +0900

    idを調べて、有無に応じて表示する言葉を変える。

--- pushMessage.php
+++ pushMessage.php
@@ -8,14 +8,12 @@ require_once('MyValidator.php');
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 $v = new MyValidator();
-
-//$message　nameのパラーメーターをメッセージとする
-$message = htmlspecialchars($_GET["name"]);
-$v->lengthCheck($message,'length',23);
-$v->regexCheck($message,'message','/(Low|High)--(USDJPY|EURJPY|GBPJPY|AUDJPY|NZDJPY|EURUSD|AUDUSD)--[0-9]{1,3}\.[0-9]{5}/');
+echo '1';
+//$accountNo　nameのパラーメーターをメッセージとする
+$accountNo = htmlspecialchars($_GET["accountNo"]);
+$v->lengthCheck($accountNo,'length',23);
+//$v->regexCheck($accountNo,'message','/(Low|High)--(USDJPY|EURJPY|GBPJPY|AUDJPY|NZDJPY|EURUSD|AUDUSD)--[0-9]{1,3}\.[0-9]{5}/');
 $v();
 
 $ids = getUserIds();
@@ -23,30 +21,36 @@ $ids = getUserIds();
 if($ids === PDO::PARAM_NULL){
   error_log('There is no id');
 }
+echo '2';
 
+bool $isRegisterd = false;
 // メッセージをユーザーID宛にプッシュ
-//$response = $bot->pushMessage($ids, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
+foreach ($ids as $id) {
+  if($id === $accountNo)
+  {
+    $isRegisterd = true;
+    break;
+  }
+}
+echo '3';
 
-// メッセージを複数人にプッシュ
-$response = $bot->multicast($ids, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message));
-
-if (!$response->isSucceeded()) {
-  error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+if ($isRegisterd)
+{
+ echo 'true';
+}
+else
+{
+  echo 'false';
 }
 
 // ユーザーIDをデータベースから取得
 function getUserIds() {
   //DBクラスを使うためにindex.phpを読み込む
   require_once('index.php');
-
   $dbh = dbConnection::getConnection();
-//  $sql = 'select userid from ' . TABLE_NAME_STONES . ' where ? = pgp_sym_decrypt(userid, \'' . getenv('DB_ENCRYPT_PASS') . '\')';
-
-  $sql = 'select pgp_sym_decrypt(userid,\''. getenv('DB_ENCRYPT_PASS') .'\') from mt4testj';
+  $sql = 'select userid from ids';
   $sth = $dbh->prepare($sql);
   $sth->execute();
-
-  //fetchAll(PDO::FETCH_COLUMN, 0); 0列目の要素を全て配列で取得
 
   // レコードが存在しなければNULL
   if (!($ids=$sth->fetchAll(PDO::FETCH_COLUMN, 0))) {
